26fbf0bbec89fd0f3e48a64d8658f7012244cd36
Araceli Coreno <113930596+amcoreno@users.noreply.github.com>
2022-10-04 17:37:25 +0000

    loops

---
+++ main.py
@@ -0,0 +1,34 @@
+# fork the repl.it and create a new git hub repo entitled
+#Comparison operators
+# Logical operators
+# Decision making
+# loops(for loops, while loops, range, enumerator)
+# min/max practice
+# Random in python
+# List comprehension
+#importing modules
+#work on creating and understanding functions
+
+# from decision_making import decisions
+# from comparison_operators import comparison
+# from dictionaries_loops import dictionaries_loops
+from loops import loop
+# from numerators import numerators
+# from lists_comprehension import lists_comprehension
+# from logical_operator import logical_operator
+# from minmax import minmax
+# from randoms import randoms
+# from range import range
+# from whileloop import whileloop
+# from zips import zips
+# decisions()
+# comparison()
+loop()
+# numerators()
+# lists_comprehension()
+# logical_operator()
+# minmax()
+# randoms()
+# range()
+# whileloop()
+# zips()
